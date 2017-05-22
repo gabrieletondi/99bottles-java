f9ff9716274a719536555fb5fe9c82b70ef76d1e
gtondi <gabriele.tondi@rumbo.com>
2017-05-22 07:43:55 +0200

    porting of exercise in java

---
+++ src/main/java/it/gabrieletondi/nintyninebottles/Bottles.java
@@ -0,0 +1,19 @@
+package it.gabrieletondi.nintyninebottles;
+
+public class Bottles
+{
+  public String verse(int verseNumber)
+  {
+    return null;
+  }
+
+  public String verse(int initialVerse, int lastVerse)
+  {
+    return null;
+  }
+
+  public String song()
+  {
+    return null;
+  }
+}
